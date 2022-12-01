3253914cacf43a50e0c71ccfa89f0dacbc47471f
ZZDirty <muztaga2@gmail.com>
2022-12-01 10:05:20 +0800

    add boot scripts

---
+++ start_node_alice.sh
@@ -0,0 +1,10 @@
+./target/release/node-template \
+--base-path ./chain/alice \
+--chain local \
+--alice \
+--port 30333 \
+--ws-port 9945 \
+--rpc-port 9933 \
+--node-key 0000000000000000000000000000000000000000000000000000000000000001 \
+--telemetry-url "ws://localhost:8001/submit 0" \
+--validator
